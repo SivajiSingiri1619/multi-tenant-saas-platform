3bf7c350536941846dd0c271718c8bd8a7b904b1
SivajiSingiri <singirisivaji@gmail.com>
2025-12-29 21:12:52 +0530

    final vercel + api url + router

--- frontend/react-app/src/services/api.js
+++ frontend/react-app/src/services/api.js
@@ -1,15 +1,25 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://multi-tenant-saas-platform-n4ho.onrender.com/api",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
+
